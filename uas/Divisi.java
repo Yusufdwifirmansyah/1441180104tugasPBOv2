469b2e90fddf982f2cef18d9df3688ab23710075
Yusufdwifirmansyah <Yusufdwif@gmail.com>
2015-07-09 10:32:49 +0700

    Uas

---
+++ uas/Divisi.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * Created by LENOVO on 06-Jul-15.
+ */
+public class Divisi {
+
+    private String namaDivisi;
+    ArrayList<Klub> daftarKlub = new ArrayList<Klub>();
+
+    public Divisi(String namaDivisi) {
+        this.namaDivisi = namaDivisi;
+    }
+
+    public String getNamaDivisi() {
+        return namaDivisi;
+    }
+
+    public void setNamaDivisi(String namaDivisi) {
+        this.namaDivisi = namaDivisi;
+    }
+
+    public ArrayList<Klub> getDaftarKlub() {
+        return daftarKlub;
+    }
+
+    public void setDaftarKlub(Klub klub) {
+        this.daftarKlub.add(klub);
+    }
+
+    @Override
+    public String toString() {
+        return "Divisi{" +
+                "namaDivisi='" + namaDivisi + '\'' +
+                '}';
+    }
+}
---
+++ uas/TestLiga.java
@@ -0,0 +1,69 @@
+/**
+ * Created by LENOVO on 06-Jul-15.
+ */
+public class TestLiga {
+
+    public static void main(String[] args) {
+
+        Liga INDONESIA = new Liga("ISL");
+
+        Divisi ISL = new Divisi("Divisi ISL");
+        Divisi Utama = new Divisi("Utama");
+
+        Klub ISL1 = new Klub("AREMAFC",ISL,INDONESIA);
+        Klub ISL2= new Klub("PERSEBAYA1997",ISL,INDONESIA);
+        Klub Utama1 = new Klub("PERSIK",Utama,INDONESIA);
+        Klub Utama2 = new Klub("PERSIWA",Utama,INDONESIA );
+
+        INDONESIA.setDaftarDivisi(ISL);
+        INDONESIA.setDaftarDivisi(Utama);
+
+        INDONESIA.setDaftarKlub(ISL1);
+        INDONESIA.setDaftarKlub(ISL2);
+        INDONESIA.setDaftarKlub(Utama1);
+        INDONESIA.setDaftarKlub(Utama2);
+
+        ISL.setDaftarKlub(ISL1);
+        ISL.setDaftarKlub(ISL2);
+        Utama.setDaftarKlub(Utama1);
+        Utama.setDaftarKlub(Utama2);
+
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Divisi : " + INDONESIA.getDaftarDivisi());
+
+        System.out.println("\n");
+
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Klub : " + INDONESIA.getDaftarKlub());
+        System.out.println("\n");
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Klub : " + Utama.getDaftarKlub());
+        System.out.println("\n");
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Klub : " + ISL.getDaftarKlub());
+        System.out.println("\n");
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Klub : " + INDONESIA.getDaftarKlub());
+        System.out.println("\n");
+        System.out.println("Liga : " + ISL1.getLiga());
+        System.out.println("Klub : " + ISL1.getNamaKlub());
+        System.out.println("Divisi : " + ISL1.getDivisi());
+        System.out.println("\n");
+        System.out.println("Liga : " + ISL2.getLiga());
+        System.out.println("Klub : " + ISL2.getNamaKlub());
+        System.out.println("Divisi : " + ISL2.getDivisi());
+        System.out.println("\n");
+        System.out.println("Liga : " + Utama1.getLiga());
+        System.out.println("Klub : " + Utama1.getNamaKlub());
+        System.out.println("Divisi : " + Utama1.getDivisi());
+        System.out.println("\n");
+        System.out.println("Liga : " + Utama2.getLiga());
+        System.out.println("Klub : " + Utama2.getNamaKlub());
+        System.out.println("Divisi : " + Utama2.getDivisi());
+        System.out.println("\n");
+
+        System.out.println("Liga : " + INDONESIA.getNamaLiga());
+        System.out.println("Klub : " + Utama.getDaftarKlub());
+        System.out.println("\n");
+}
+}
